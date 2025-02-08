0cba950294c64d2d236ed6e3bdbaac090c3564a4
Amarnadh001 <165272726+Amarnadh001@users.noreply.github.com>
2025-02-09 00:52:53 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
-
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://del-exscel-backend8.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
