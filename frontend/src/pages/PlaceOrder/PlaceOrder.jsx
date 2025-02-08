5e2a160c081c2fafccd3cfa25d7d364252b5a14e
Amarnadh001 <99220041612@klu.ac.in>
2025-02-09 01:04:17 +0530

    fourth

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { StoreContext } from '../../context/storeContext'
+import { StoreContext } from '../../context/StoreContext'
 import './PlaceOrder.css'
 
 const PlaceOrder = () => {
